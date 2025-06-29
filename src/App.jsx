866ac161310b7c2fb97b187307e7c6e03565f28f
Naman Taggar <95559793+zplus11@users.noreply.github.com>
2025-06-29 19:21:09 +0530

    Initial Upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import AdminLogin from "./components/AdminLogin";
 
